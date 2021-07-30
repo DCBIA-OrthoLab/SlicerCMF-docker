ef5d67facfd2d293157f7ca7e5ef837ff8b8060d
Celia Le <luciacev-admin@0587379925.wireless.umich.net>
2021-07-30 17:25:13 -0400

    ADD: Signal to Noise Ratio with separated dockerfile (to save time while building the slicercmf-docker)

---
+++ src/sh/SignalNoiseRatio.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+Help()
+{
+# Display Help
+
+echo "-h|--help                 Print this Help."
+echo
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --before_dir )  shift
+            before_dir=$1;;
+        --after_dir )  shift
+            after_dir=$1;;
+        --outfile )  shift
+            outfile=$1;;
+
+        -h | --help )
+            Help
+            exit;;
+        * ) 
+            echo ' - Error: Unsupported flag'
+            Help
+            exit 1
+    esac
+    shift
+done
+
+# before_dir=$1
+# after_dir=$2
+# outfile=$3
+outfile="${outfile:-SNR.txt}"
+
+before_files=( $before_dir/* )
+after_files_dirs=( $after_dir/* )
+
+while [ "${#before_files[@]}" -gt 0 ] &&
+      [ "${#after_files_dirs[@]}" -gt 0 ]
+do
+
+    after_files=( ${after_files_dirs[0]}/* )
+
+    while [ "${#after_files[@]}" -gt 0 ]
+    do
+        echo $(basename ${before_files[0]}) >> $outfile
+        echo $(basename ${after_files[0]}) >> $outfile
+
+        /app/build_ImageNoise/bin/ImageNoise ${before_files[0]} -s ${after_files[0]} -i ${before_files[0]} >> $outfile
+        
+        echo >> $outfile
+
+        after_files=( "${after_files[@]:1}" )
+    done
+
+    before_files=( "${before_files[@]:1}" )
+    after_files_dirs=( "${after_files_dirs[@]:1}" )
+done
+
+echo "$outfile saved"
